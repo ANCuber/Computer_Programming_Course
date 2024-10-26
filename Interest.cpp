719c906c2815ea3fc52bd9fcffa9fdfff971f147
ancuber1031 <ansona1131@gmail.com>
2024-10-26 16:19:21 +0800

    midterm

--- Interest.cpp
+++ Interest.cpp
@@ -34,7 +34,7 @@ bool simulate_payment_2(long long LL, long long PP) {
     for (int i = 0; i < 132; ++i) {
         if (i < 6) L += (L/1000)*3;
         else L += (L/1000)*2;
-        if (L%100) L += (100-L%100);
+        if (L%1000) L += (1000-L%1000);
         L -= P;
         if (L <= 0) return true;
     }
@@ -55,16 +55,18 @@ long long calculate_min_payment_2(long long L) {
 int main() {
     long long cnt = 0, bs = 1000;
     srand(time(NULL));
-    for (int ndx = 0; ndx < 10; ndx ++) {
+    for (int ndx = 0; ndx <= 13; ndx ++) {
         long long a = rand(), b = rand(), c = rand();
         long long L = bs;
         long long P1 = calculate_min_payment_1(L);
         long long P2 = calculate_min_payment_2(L);
-        if (P1 != P2) {
+        /*if (P1 != P2) {
             cout<<P1<<' '<<P2<<endl;
             cnt++;
-        }
+        }*/
+        cout<<bs<<' '<<P1<<endl;
+        cout<<bs<<' '<<P2<<endl;
         bs *= 10;
     }
-    cout<<cnt<<endl;
+    //cout<<cnt<<endl;
 }
